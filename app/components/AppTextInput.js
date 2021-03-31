e6102a09fe667c7ed7a0ad984f0b8efaf83d2884
Ali Nakhaee <ali.sh7097@gmail.com>
2021-03-31 20:23:54 +0430

    first splash screen and login page

---
+++ app/components/AppTextInput.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+import AppText from "./AppText";
+
+import Svg from "react-native-svg";
+import UserIcon from "./icons/UserIcon";
+
+function AppTextInput({ label, required, LeftIcon, RightIcon }) {
+  return (
+    <View>
+      <AppText style={styles.label}>{label + (required ? " *" : "")}</AppText>
+      <View style={styles.container}>
+        {LeftIcon}
+        <TextInput style={styles.textInput} />
+        {RightIcon}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+  textInput: {
+    marginLeft: 10,
+    flex: 1,
+    direction: "rtl",
+  },
+  label: {
+    alignSelf: "flex-end",
+    fontSize: 10,
+    paddingHorizontal: 10,
+  },
+});
+
+export default AppTextInput;

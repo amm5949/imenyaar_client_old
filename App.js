9bb26e30dc5ebe238eeb2c016fdeb3e0535f7812
Meysam <mameysam23@outlook.com>
2021-04-02 16:29:22 +0430

    rename

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
 import Splash from "./app/screens/splash/Splash";
-import LogInScreen from "./app/screens/LogIn Screen/LogInScreen";
-import ForgetPasswordScreen from "./app/screens/LogIn Screen/ForgetPasswordScreen";
-import ForgetPasswordSecurityCodeScreen from "./app/screens/LogIn Screen/ForgetPasswordSecurityCodeScreen";
+import LogInScreen from "./app/screens/login/LogInScreen";
+import ForgetPasswordScreen from "./app/screens/login/ForgetPasswordScreen";
+import ForgetPasswordSecurityCodeScreen from "./app/screens/login/ForgetPasswordSecurityCodeScreen";
 
 export default function App() {
-  return <Splash />;
+  return <ForgetPasswordSecurityCodeScreen />;
 }
 
 const styles = StyleSheet.create({

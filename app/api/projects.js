9301f6e9a310fc4b6f4a94249eec3a7c350237b6
Amir Mohammad Maghrebi <amm5949@gmail.com>
2021-12-31 21:42:43 +0330

    change the getProject to apidoc style

--- app/api/projects.js
+++ app/api/projects.js
@@ -3,8 +3,17 @@ import client from "./client";
 
 const endpoint = "/api/projects";
 
-export const getProject = async () => {
-    const loginResponse = await loginToGetToken();
+export const getProject = async ( project ) => {
+    const loginResponse = await client.get(endpoint + "/login", {
+        id: project.id,
+        name: project.name,
+        owner_id: project.owner_id,
+        start_date: project.start_date,
+        scheduled_end: project.project,
+        address: project.address,
+        area: project.area,
+        is_multizoned: project.is_multizoned,
+    })
     const access_token = loginResponse.result.tokens.access_token;
 
     return client.get(endpoint, {

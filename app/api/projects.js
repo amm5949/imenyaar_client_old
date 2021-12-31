56155788e992006d7522c95b5857deb420d736ba
Amir Mohammad Maghrebi <amm5949@gmail.com>
2022-01-01 00:18:30 +0330

    back to previous forms

--- app/api/projects.js
+++ app/api/projects.js
@@ -1,19 +1,11 @@
 import { loginToGetToken } from "./auth";
 import client from "./client";
+// why /:id is 3?
+const endpoint = "/api/projects/3";
 
-const endpoint = "/api/projects";
-
-export const getProject = async ( project ) => {
-    const loginResponse = await client.get(endpoint + "/login", {
-        id: project.id,
-        name: project.name,
-        owner_id: project.owner_id,
-        start_date: project.start_date,
-        scheduled_end: project.project,
-        address: project.address,
-        area: project.area,
-        is_multizoned: project.is_multizoned,
-    })
+// fetch projects
+export const getProject = async () => {
+    const loginResponse = await loginToGetToken();
     const access_token = loginResponse.result.tokens.access_token;
 
     return client.get(endpoint, {

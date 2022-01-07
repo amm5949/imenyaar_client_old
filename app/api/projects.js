02570dad3056625bfcd72ca0747783d23e0add53
Amir Mohammad Maghrebi <amm5949@gmail.com>
2022-01-08 00:13:50 +0330

    add fetch method to project.js

--- app/api/projects.js
+++ app/api/projects.js
@@ -1,7 +1,7 @@
 import { loginToGetToken } from "./auth";
 import client from "./client";
 // why /:id is 3?
-const endpoint = "/api/projects/3";
+const endpoint = "/api/projects";
 
 // fetch projects
 export const getProject = async () => {
@@ -12,3 +12,12 @@ export const getProject = async () => {
         headers: { Authorization: "Bearer " + access_token },
     });
 };
+
+export const fetchProject = async() => {
+    const loginResponse = await loginToGetToken();
+    const access_token = loginResponse.result.access_token;
+    // access_token.
+    return client.get(endpoint + "/:3", {
+        headers: { Authorization: "Bearer " + access_token},
+    })
+}

63b9a2f3a1d97fe6930308a626dd643c8a2dd47d
alasti <devnerds2020@gmail.com>
2022-05-26 22:46:34 +0430

    fixing a bug

--- app/api/questions/update.js
+++ app/api/questions/update.js
@@ -2,7 +2,7 @@ import client from "../client";
 
 const endpoint = "/api/questions"
 
-export const getQuestion = async (category_id) => {
+export const updateQuestion = async (category_id) => {
     const endpoint = `/api/questions/order/:${category_id}`
     const response = await client.put(endpoint, {
         headers: { Authorization: "Bearer " + access_token },

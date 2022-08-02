cdc99253313083ba120444ab9b4291fd0750863d
amirreza alasti <alasti_amirreza@yahoo.com>
2022-08-02 23:12:12 +0430

    fix some styles

--- app/screens/accidents/AccidentDetailsScreen.style.js
+++ app/screens/accidents/AccidentDetailsScreen.style.js
@@ -1,4 +1,4 @@
-import { Dimensions , StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import colors from "../../config/colors";
 
 
@@ -7,77 +7,85 @@ const windowHeight = Dimensions.get("window").height;
 const fontScale = Dimensions.get("window").fontScale;
 
 const styles = StyleSheet.create({
-    cardView: {
-      flexDirection: "row",
-      justifyContent: "space-evenly",
-      marginBottom: 18,
-    },
+  cardView: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginBottom: 18,
+  },
 
-    
-    container: {
-      backgroundColor: "#201a31",
-      justifyContent: "space-between",
-      height: 1 * windowHeight,
-    },
-    descriptionView: {
-      flexDirection: "row",
-      justifyContent: "flex-end",
-      alignItems: "center",
-      marginBottom: 19,
-    },
-    detailsView: {
-      width: "100%",
-      alignItems: "center",
-      paddingHorizontal: 15,
-      backgroundColor: colors.white,
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-      position: "absolute",
-      bottom: 0,
-      paddingTop: 10,
-      maxHeight: 0.832 * windowHeight,
-    },
-    headerView: {
-      flexDirection: "row",
-      width: "100%",
-      justifyContent: "flex-end",
-      marginBottom: 10,
-    },
-    headerText: {
-      fontSize: 14 / fontScale,
-      color: colors.black,
-      marginRight: 10,
-    },
-    imageBackground: {
-      width: "100%",
-      height: 0.85 * windowHeight,
-      // marginBottom: 20,
-      alignItems: "center",
-    },
-    imageSectionHeaderText: {
-      fontSize: 10.5 / fontScale,
-      width: "80%",
-      color: "#7a7c83",
-      marginRight: 10,
-    },
-    imageSectionHeaderView: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "flex-end",
-      marginBottom: 8,
-    },
-    text: {
-      fontSize: 10.5 / fontScale,
-      textAlign: "right",
-      width: "84%",
-      marginRight: 10,
-      color: "#333",
-      color: colors.errorRed,
-    },
-    textView: {
-      marginTop: 15,
-      marginBottom: 10,
-    },
-  });
+  container: {
+    backgroundColor: "#201a31",
+    justifyContent: "space-between",
+    height: 1 * windowHeight,
+  },
 
-  export {styles};
+  descriptionView: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 19,
+  },
+
+  detailsView: {
+    width: "100%",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    backgroundColor: colors.white,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    position: "absolute",
+    bottom: 0,
+    paddingTop: 10,
+    maxHeight: 0.832 * windowHeight,
+  },
+
+  headerView: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "flex-end",
+    marginBottom: 10,
+  },
+
+  headerText: {
+    fontSize: 14 / fontScale,
+    color: colors.black,
+    marginRight: 10,
+  },
+
+  imageBackground: {
+    width: "100%",
+    height: 0.85 * windowHeight,
+    // marginBottom: 20,
+    alignItems: "center",
+  },
+
+  imageSectionHeaderText: {
+    fontSize: 10.5 / fontScale,
+    width: "80%",
+    color: "#7a7c83",
+    marginRight: 10,
+  },
+
+  imageSectionHeaderView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    marginBottom: 8,
+  },
+
+  text: {
+    fontSize: 10.5 / fontScale,
+    textAlign: "right",
+    width: "84%",
+    marginRight: 10,
+    color: "#333",
+    color: colors.errorRed,
+  },
+
+  textView: {
+    marginTop: 15,
+    marginBottom: 10,
+  },
+});
+
+export { styles };
--- app/screens/projects/ProjectListScreen.style.js
+++ app/screens/projects/ProjectListScreen.style.js
@@ -1,4 +1,4 @@
-import { Dimensions , StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import colors from "../../config/colors";
 import { useState } from "react";
 
@@ -9,87 +9,84 @@ const fontScale = Dimensions.get("window").fontScale;
 
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.inputViewBackground,
+    flex: 1,
+    alignItems: "center",
+  },
 
 
+  projectKindContainer: {
+    borderRadius: 13,
+    borderColor: "#003f5c",
+    borderWidth: 1,
+    width: "84.6%",
+    flexDirection: "row",
+    overflow: "hidden",
+    marginVertical: 10,
+  },
 
-    container: {
-      backgroundColor: colors.inputViewBackground,
-      flex: 1,
-      alignItems: "center",
-    },
+  // unactiveProjectContainer: {
+  //     paddingVertical: 5,
+  //     backgroundColor: active ? colors.inputViewBackground : "#003f5c",
+  //     alignItems: "center",
+  //     flex: 1,
+  // },
 
+  // unactiveProjectText: {
+  //     fontSize: 11 / fontScale,
+  //     color: active ? "#003f5c" : colors.inputViewBackground,
+  // },
 
-    projectKindContainer: {
-        borderRadius: 13,
-        borderColor: "#003f5c",
-        borderWidth: 1,
-        width: "84.6%",
-        flexDirection: "row",
-        overflow: "hidden",
-        marginVertical: 10,
-    },
+  // activeProjectContainer: {
+  //     paddingVertical: 5,
+  //     backgroundColor: active ? "#003f5c" : colors.inputViewBackground,
+  //     alignItems: "center",
+  //     flex: 1,
+  // },
 
-    // unactiveProjectContainer: {
-    //     paddingVertical: 5,
-    //     backgroundColor: active ? colors.inputViewBackground : "#003f5c",
-    //     alignItems: "center",
-    //     flex: 1,
-    // },
+  // activeProjectText: {
+  //     fontSize: 11 / fontScale,
+  //     color: active ? colors.inputViewBackground : "#003f5c",
+  // },
 
-    // unactiveProjectText: {
-    //     fontSize: 11 / fontScale,
-    //     color: active ? "#003f5c" : colors.inputViewBackground,
-    // },
+  unactiveProjectDetails: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1
+  },
 
-    // activeProjectContainer: {
-    //     paddingVertical: 5,
-    //     backgroundColor: active ? "#003f5c" : colors.inputViewBackground,
-    //     alignItems: "center",
-    //     flex: 1,
-    // },
+  activeProjectPage: {
+    width: "100%",
+    flex: 1,
+  },
 
-    // activeProjectText: {
-    //     fontSize: 11 / fontScale,
-    //     color: active ? colors.inputViewBackground : "#003f5c",
-    // },
+  addNewProjectButton: {
+    alignSelf: "flex-end",
+    marginBottom: 10,
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
 
-    unactiveProjectDetails: {
-        justifyContent: "center",
-        alignItems: "center",
-        flex: 1
-    },
+  emptyListImage: {
+    width: 0.87 * windowWidth,
+    height: 0.29 * windowHeight,
+    marginTop: 0.055 * windowHeight,
+    marginBottom: 15,
+  },
 
-    activeProjectPage: {
-        width: "100%",
-        flex: 1,
-    },
+  notFoundText: {
+    fontSize: 15 / fontScale,
+    color: colors.darkBlue,
+  },
 
-    addNewProjectButton: {
-        alignSelf: "flex-end",
-        marginBottom: 10,
-        position: "absolute",
-        bottom: 10,
-        right: 10,
-    },
+  textContainer: {
+    width: "100%",
+    alignItems: "center",
+    flex: 1,
+    marginVertical: 20,
+  },
+});
 
-    emptyListImage: {
-      width: 0.87 * windowWidth,
-      height: 0.29 * windowHeight,
-      marginTop: 0.055 * windowHeight,
-      marginBottom: 15,
-    },
-
-    notFoundText: {
-      fontSize: 15 / fontScale,
-      color: colors.darkBlue,
-    },
-
-    textContainer: {
-      width: "100%",
-      alignItems: "center",
-      flex: 1,
-      marginVertical: 20,
-    },
-  });
-
-  export {styles};
+export { styles };

4599d3fc702ca5e1b4695fbd87b1eadf6bf21567
Amir Mohammad Maghrebi <amm5949@gmail.com>
2022-08-12 12:38:39 +0430

    update api for users updated

--- app/api/people/update.js
+++ app/api/people/update.js
@@ -1,7 +1,7 @@
 import client from "../client";
 
 export const updatePeople = async (access_token, id, data) => {
-  const endpoint = `/api/users/${id}`;
+  const endpoint = `/api/users/:${id}`;
   const response = await client.put(endpoint, data, {
     headers: { Authorization: "Bearer " + access_token },
   });
